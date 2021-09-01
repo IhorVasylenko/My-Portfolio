de7c59790570c2f468fbd458a312f9285158ecfb
IhorVasylenko <theone116355@gmail.com>
2021-09-01 12:12:55 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import style from './App.module.css';
+import {Main} from "./main/Main";
+import {Skills} from "./skills/Skills";
+import {AboutMe} from "./about-me/AboutMe";
+import {Portfolio} from "./portfolio/Portfolio";
+import {Contact} from "./contact/Contact";
+import ReactPageScroller from 'react-page-scroller';
+import HamburgerMenu, {MenuPositionType} from "./hamburgerMenu/HamburgerMenu";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [currentPage, setCurrenPage] = useState(0)
+
+    const positionsData: MenuPositionType[] = [
+        {id: '1', title: 'Home', active: false, path: setCurrenPage},
+        {id: '2', title: 'AboutMe', active: false, path: setCurrenPage},
+        {id: '3', title: 'Skills', active: false, path: setCurrenPage},
+        {id: '4', title: 'Portfolio', active: false, path: setCurrenPage},
+        {id: '5', title: 'Contact', active: false, path: setCurrenPage},
+    ]
+
+    return (
+        <div className={style.appContainer}>
+            <div className={style.hamburgerMenu}>
+                <HamburgerMenu id={'menu'} menuPositions={positionsData}/>
+            </div>
+            <ReactPageScroller customPageNumber={currentPage}>
+                <Main/>
+                <AboutMe/>
+                <Skills/>
+                <Portfolio/>
+                <Contact/>
+            </ReactPageScroller>
+        </div>
+    );
 }
 
 export default App;
